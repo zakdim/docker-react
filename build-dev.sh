3aff9c5f0c5117eada2e8f41204c43cc1e57b751
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-04-19 16:50:43 -0400

    7-93 A Touch More Travis Setup

--- build-dev.sh
+++ build-dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Specify docker file other than Dockerfile with -f option
-docker build -f Dockerfile.dev .
+# 7-93: A Touch More Travis Setup
+docker build -t zakdim/docker-react -f Dockerfile.dev .
--- run-test.sh
+++ run-test.sh
@@ -7,5 +7,12 @@
 # docker-compose up
 # Than attach to the running container and run tests
 # docker exec -it <CONTAINER_ID> npm run test
-docker exec -it 694fbd54b37f npm run test
+# docker exec -it 694fbd54b37f npm run test
+
+# 7-93: A Touch More Travis Setup
+# docker run zakdim/docker-react npm run test -- --coverage
+
+# 7-92: Fix for Failing Travis Builds
+# docker run -e CI=true zakdim/docker-react npm run test
+docker run -e CI=true zakdim/docker-react npm run test -- --coverage
 
