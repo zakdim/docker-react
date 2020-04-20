a6f1c557d6049d8a94c88e24e6d95331cc725593
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-04-19 20:22:57 -0400

    7-102 Workflow With Github

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>I was changed!</p>
+        <p>I was changed on the feature branch!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
